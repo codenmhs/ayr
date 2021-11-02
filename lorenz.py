e9dc5f6025ebba0e27dfe5d5073bbef4fe2b76a0
Wyatt Homola <whomola@cvsu.org>
2021-11-02 13:09:15 -0400

    Make instructions clearer

--- lorenz.py
+++ lorenz.py
@@ -3,27 +3,30 @@
 '''
 
 import numpy as np
-import matplotlib.pyplot as plt
 from scipy.integrate import odeint
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
+from ipywidgets import interact
 
-rho = 28.0
-sigma = 10.0
-beta = 8.0 / 3.0
+mpl.rcParams['figure.figsize'] = (10,10)
 
-def f(state, t):
-    x, y, z = state  # Unpack the state vector
-    return sigma * (y - x), x * (rho - z) - y, x * y - beta * z  # Derivatives
+@interact(rho=(20, 30), beta=(0.1, 3), sigma=(0.1, 15))
 
-state0 = [1.0, 1.0, 1.0]
-t = np.arange(0.0, 40.0, 0.001)
+def plot_trajectory(rho=28.0, beta=8.0 / 3, sigma=10.0): 
 
-states = odeint(f, state0, t)
+    def f(state, t):
+        x, y, z = state  # Unpack the state vector
+        return sigma * (y - x), x * (rho - z) - y, x * y - beta * z  # Derivatives
 
-fig = plt.figure()
-ax = fig.gca(projection="3d")
-# ax.plot(states[:, 0], states[:, 1], states[:, 2])
-ax.scatter(states[:, 0], states[:, 1], states[:, 2], c=cm.cool(np.abs(t / 50)), s=0.5)
-plt.draw()
-plt.show()
+    state0 = [1.0, 1.0, 1.0]
+    t = np.arange(0.0, 40.0, 0.001)
+
+    states = odeint(f, state0, t)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+    ax.scatter(states[:, 0], states[:, 1], states[:, 2], c=cm.cool(np.abs(t / 50)), s=0.5)
+    plt.draw()
+    plt.show()
